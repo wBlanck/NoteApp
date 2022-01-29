4c512246f42aa8006c74c80cdf7db814ccf2c2f0
wblanck - laptop <blanck.william@gmail.com>
2022-01-30 00:40:21 +0100

    boilerplate

---
+++ src/components/NotesList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Note from "./Note";
+
+function NotesList() {
+  return (
+    <div className="notes-list">
+      noteList
+      <Note />
+    </div>
+  );
+}
+
+export default NotesList;
