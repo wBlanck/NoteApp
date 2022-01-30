36d2cbff43630960192ff8679ccfc69eba80ac92
wblanck - laptop <blanck.william@gmail.com>
2022-01-30 08:12:40 +0100

    added modal slide in animation

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 
 function SearchBar() {
+  const slide = () => {
+    const modal = document.querySelector(".modal");
+    modal.classList.add("slide-in");
+  };
   return (
-    <input type="text" placeholder="Search Notes..." className="search-bar" />
+    <input
+      type="text"
+      placeholder="Search Notes..."
+      className="search-bar"
+      onClick={slide}
+    />
   );
 }
 
