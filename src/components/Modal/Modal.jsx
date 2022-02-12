c9e57a3d5e6bb20b969b7e359f6037ed93f8316b
wblanck <blanck.william@gmail.com>
2022-02-12 12:46:54 +0100

    wip

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,14 +3,38 @@ import Container from "../Container/Container";
 import "./Modal.scss";
 
 function Modal({ content, addNote }) {
-  console.log(content);
-  return (
-    <div className={`modal`}>
-      <Container>
-        <Button type="add" handleClick={addNote} />
-      </Container>
-    </div>
-  );
+  switch (content) {
+    case "addNote":
+      return (
+        <div className={`modal`}>
+          <input type="text" />
+
+          <Container>
+            <textarea></textarea>
+            <Button type="add" handleClick={addNote} />
+          </Container>
+        </div>
+      );
+    case "addFolder":
+      return (
+        <div className={`modal`}>
+          <Container>
+            AddFolder
+            <Button type="add" handleClick={addNote} />
+          </Container>
+        </div>
+      );
+
+    default:
+      return (
+        <div className={`modal`}>
+          <Container>
+            do something
+            <Button type="add" />
+          </Container>
+        </div>
+      );
+  }
 }
 
 export default Modal;
